905531e0b9f9bdd736e61a8ab7ece0d781a6f8c7
lfreeman0820 <lfreeman0820@gmail.com>
2020-08-05 16:33:11 -0700

    ok

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 
 //important! connect to mongoose database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.LONNI_MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
